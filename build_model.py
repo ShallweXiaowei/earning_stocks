7cd477a072ef1cfe3eeca7b559dfa43f7d4842d8
Xiaowei <shufeyxw@gmail.com>
2019-08-01 22:04:27 -0400

    added analysis

--- build_model.py
+++ build_model.py
@@ -5,36 +5,107 @@ Created on Wed Jul 24 17:29:06 2019
 @author: shufe
 """
 
-from get_stock_price import get_daily, calc_return
+from get_stock_price import calc_ret_from_file, get_price_from_files
 import os
 import pandas as pd
-import time
-from datetime import datetime
-
+import numpy as np
 
 earning_folder = os.getcwd() + '/earnings/'
 files = os.listdir(earning_folder)
 
-df = pd.read_csv(earning_folder + files[-3],index_col = 0, parse_dates = True)
+def concat_earning(n):
+    res = pd.DataFrame()
+    for i in files[-n:]:
+        df = pd.read_csv(earning_folder + i,index_col = 0, parse_dates = True)
+        res = pd.concat([res,df])
+    return res
 
-def add_ret_column(df_input,col_name,start_key, end_n, end_key):
+def add_ret_column(df_input,col_name,start_n, start_key, end_n, end_key):
     col = []
     for i, row in df_input.iterrows():
         try:
             symbol = str(row['symbol']).strip()
-            api = '8061RWJT6KZZKZMP'
-            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=%s&outputsize=%s&apikey=%s&datatype=csv'%(symbol,'full',api)
-            price_df = pd.read_csv(url)
-            price_df.index = price_df['timestamp'].map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
-            price_df = price_df.drop(['timestamp'], axis = 1)
-            ret = calc_return(price_df, row.name.date(),start_key,end_n,end_key)
+            ret = calc_ret_from_file(symbol, row.name.date(),start_n, start_key,end_n,end_key)
             col.append(ret)
         except:
-            print ('%s is bad'%row['symbol'])
-            col.append('NA')
-    df_input[col_name] = col
+           # print ('%s is bad'%row['symbol'])
+            col.append(np.nan)
+    df_input.loc[:,col_name] = col
+    return df_input
+
+
+def add_price_column(df_input, col_name, start_n, start_key):
+    col = []
+    for i, row in df_input.iterrows():
+        try:
+            symbol = str(row['symbol']).strip()
+            ret = get_price_from_files(symbol, row.name.date(),start_n, start_key)
+            col.append(ret)
+        except:
+           # print ('%s is bad'%row['symbol'])
+            col.append(np.nan)
+    df_input.loc[:,col_name] = col
     return df_input
 
 
 
-df = add_ret_column(df, 'day0ret', 'open', 0,'adjusted_close')
+def clean_cap(x):
+    if str(x) == 'nan':
+        return np.nan
+    else:
+        return int(str(x).replace(',',''))
+def clean_est(x):
+    if str(x) == '--':
+        return np.nan
+    else:
+        return float(x)
+
+if __name__ == '__main__':
+    df = concat_earning(100)
+    df['cap'] = df['cap'].map(lambda x: clean_cap(x))
+    df['est'] = df['est'].map(lambda x: clean_est(x))
+    df['report'] = df['report'].map(lambda x: clean_est(x))
+    df['surp'] = df['report'] - df['est']
+    df['pct_surp'] = df['report']/df['est'] - 1
+    
+    before = df[df['timing'] == 'bmo']
+    before = add_ret_column(before,'day0ret', -1, 'close', 0,'adjusted_close' )
+    before = add_ret_column(before, 'day0close_open', 0,'open',0,'adjusted_close')
+    before = add_ret_column(before,  'ret 3',0, 'open', 2,'adjusted_close')
+    
+    after = df[df['timing'] == 'amc']
+    after = add_ret_column(after, 'day0ret', 0,'adjusted_close', 1,'adjusted_close')
+    after = add_ret_column(after, 'day0close_open',1, 'open', 1,'adjusted_close')
+    after = df = add_ret_column(after, 'ret 3',1, 'open', 3,'adjusted_close')
+    
+    cob = pd.concat([before, after])
+    cob = cob.dropna()
+
+
+'''-------------------------------------- Analysis ------------------------------------------------------'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- get_stock_price.py
+++ get_stock_price.py
@@ -7,7 +7,9 @@ Created on Thu Jul 11 22:05:56 2019
 
 import pandas as pd
 from datetime import datetime
+import os
 
+data_path = os.getcwd() + '/stock_data/'
 
 def get_daily(ticker, size = 'full'):
     api = '8061RWJT6KZZKZMP'
@@ -27,12 +29,42 @@ def get_intra(ticker, interval = '1min'):
 
 
 def calc_return(ret_df, start_date,start_key,end_n, end_key):
+    ret_df = ret_df.sort_index(ascending = True)
     sub_df = ret_df[start_date:]
     start = sub_df.iloc[0][start_key]
     end = sub_df.iloc[:end_n + 1].iloc[-1][end_key]
     return end/start - 1
-    
 
+def calc_ret_from_file(symbol, start_date,start_n,start_key,end_n, end_key):
+    ret_df = pd.read_csv(data_path + symbol + '.csv', index_col = 0,parse_dates = True)
+    ret_df = ret_df.sort_index(ascending = True)
+    sub_df = ret_df[start_date:]
+
+    if start_n <0:
+        add = ret_df[:start_date].iloc[start_n-1:]
+        start = add.iloc[0][start_key]
+    else:
+        start = sub_df.iloc[start_n:].iloc[0][start_key]
+        
+    end = sub_df.iloc[:end_n + 1].iloc[-1][end_key]
+    return end/start - 1 
+
+
+def get_price_from_files(symbol, start_date, start_n, start_key):
+    ret_df = pd.read_csv(data_path + symbol + '.csv', index_col = 0,parse_dates = True)
+    ret_df = ret_df.sort_index(ascending = True)
+    sub_df = ret_df[start_date:]
+
+    if start_n <0:
+        add = ret_df[:start_date].iloc[start_n-1:]
+        start = add.iloc[0][start_key]
+    else:
+        start = sub_df.iloc[start_n:].iloc[0][start_key]
+        
+    return start
+    
+    
+    
 if __name__ == '__main__':
     raw_input = input()
     df = get_daily(raw_input)
